36422777a24d552262ec59942b9f3a62774e6350
Stephane Gillon <stephanegillon@hotmail.com>
2022-05-12 11:55:46 +0200

    tutorial part 3 finish

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input} from '@angular/core';
+import { FaceSnap } from "../models/face-snap.model";
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss']
+})
+export class FaceSnapComponent implements OnInit{
+  @Input() faceSnap!: FaceSnap;
+
+  buttonText!: string;
+
+  ngOnInit(){
+    this.buttonText = 'like'
+  }
+
+  onSnap() {
+    if (this.buttonText === 'like') {
+      this.faceSnap.snaps++;
+      this.buttonText = 'unlike!';
+    } else {
+      this.faceSnap.snaps--;
+      this.buttonText = 'like';
+    }
+  }
+}
